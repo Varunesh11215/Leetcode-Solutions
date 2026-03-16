bf0b070d19b50f7eac1bb633c2fde1ad476775c3
Varunesh11215 <23it1122@stjosephstechnology.ac.in>
2026-03-16 12:05:16 +0530

    Time: 15 ms (63.5%), Space: 51.7 MB (15.17%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int count=0;
+        int j=s.length-1;
+        for(int i=g.length-1;i>=0&&j>=0;i--){
+            if(g[i]<=s[j]){
+                count++;
+                j--;
+            }
+        }
+        return count;
+    }
+}
