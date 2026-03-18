9ca5a5752928782595177a3ae84377fe05b2f72e
Varunesh11215 <23it1122@stjosephstechnology.ac.in>
2026-03-18 10:25:23 +0530

    Time: 13 ms (11.43%), Space: 46.9 MB (98.82%) - LeetHub

---
+++ 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people,(a,b)->{
+            if(a[0]==b[0]){
+                return a[1]-b[1];
+            }
+            return b[0]-a[0];
+        });
+        int len=people.length;
+        for(int i=0;i<len;i++){
+            int target=people[i][1];
+            int t1=people[i][1];
+            int t2=people[i][0];
+            if(i>target){
+                for(int j=i-1;j>=target;j--){
+                    people[j+1][1]=people[j][1];
+                    people[j+1][0]=people[j][0];
+                }
+            }
+            people[target][0]=t2;
+            people[target][1]=t1;
+        }
+        return people;
+    }
+}
