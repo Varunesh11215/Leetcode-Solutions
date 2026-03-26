1ffb067887968b70efefb56fadf3bcd77ea6625e
Varunesh11215 <23it1122@stjosephstechnology.ac.in>
2026-03-26 10:27:17 +0530

    Time: 19 ms (36.22%), Space: 48.1 MB (13.01%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minDeletions(String s) {
+        int len=s.length();
+        int arr[]=new int[26];
+        for(int i=0;i<len;i++){
+            arr[s.charAt(i)-'a']++;
+        }
+
+        Set<Integer> set = new HashSet<>();
+        Arrays.sort(arr);
+        for(int i:arr) System.out.println(i);
+        int count=0;
+
+        set.add(arr[25]);
+
+        for(int i=24;i>=0;i--){
+            if(arr[i]==0){
+                break;
+            }
+            if(set.contains(arr[i])){
+                int min = Collections.min(set) - 1;
+                count += arr[i] - min;
+                arr[i] = min;
+            }
+
+            if(arr[i] != 0 ) set.add(arr[i]);
+        }
+        return count;
+    }
+}
