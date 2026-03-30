503bfdafb0bed9ed0f9692ea8bb3e0f34f432b32
Varunesh11215 <23it1122@stjosephstechnology.ac.in>
2026-03-30 11:55:20 +0530

    Time: 19 ms (52.58%), Space: 97.7 MB (94.06%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,25 +1,18 @@
 class Solution {
-    public static void poss(List<List<Integer>>list,List<Integer>li,int k,int nums[],int i){
-        if(i==nums.length){
-            return;
-        }
-        if(li.size()==k){
-            list.add(new ArrayList<>(li));
-            return;
-        }
-        li.add(nums[i]);
-        poss(list,li,k,nums,i+1);
-        li.remove(li.size()-1);
-        poss(list,li,k,nums,i+1);
-    }
     public List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>>list=new ArrayList<>();
-        List<Integer> li=new ArrayList<>();
-        int nums[]=new int[n+1];
-        for(int i=0;i<=n;i++){
-            nums[i]=i+1;
+        List<List<Integer>> res = new ArrayList<>();
+        function(1, n, k, res, new ArrayList<>());
+        return res;
+    }
+    public void function(int start, int n, int k,List<List<Integer>> res,List<Integer> list) {
+        if (list.size() == k) {
+            res.add(new ArrayList<>(list));
+            return;
         }
-        poss(list,li,k,nums,0);
-        return list;
+        for (int i = start; i <= n; i++) {
+            list.add(i);
+            function(i + 1, n, k, res, list);
+            list.remove(list.size() - 1);
+        }
     }
 }
