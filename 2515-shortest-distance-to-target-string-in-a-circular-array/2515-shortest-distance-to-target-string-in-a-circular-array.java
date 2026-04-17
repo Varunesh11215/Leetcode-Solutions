25bad001f7001758f4b5615fc5bd5a243d5bd4af
Varunesh11215 <23it1122@stjosephstechnology.ac.in>
2026-04-17 10:01:13 +0530

    Time: 1 ms (20.1%), Space: 46.9 MB (36.07%) - LeetHub

---
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int closestTarget(String[] words, String target, int startindex) {
+        int n=words.length;
+        int fc=0;
+        int i=startindex;
+        if(words[i].equals(target)){
+            return 0;
+        }
+        i=(i+1)%n;
+        fc++;
+        while(i!=startindex){
+            if(words[i].equals(target)){
+                break;
+            }
+            fc++;
+            i=(i+1)%n;
+        }
+        int bc=0;
+        i=startindex;
+        i=(i-1+n)%n;
+        bc++;
+        while(i!=startindex){
+            if(words[i].equals(target)){
+                break;
+            }
+            bc++;
+            i=(i-1+n)%n;
+        }
+        int res=Math.min(fc,bc);
+        if(res==n){
+            return -1;
+        }
+        return res;
+    }
+}
