309a610f9ff96c93b684bc1cae27a79d9b15dd8b
Varunesh11215 <23it1122@stjosephstechnology.ac.in>
2026-05-27 19:51:24 +0530

    Time: 1 ms (98.05%), Space: 42.8 MB (24.5%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int addDigits(int num) {
+        while(num>=10){
+            int t=0;
+            while(num>0){
+                t+=num%10;
+                num/=10;
+            }
+            num=t;
+        }
+        return num;
+    }
+}
