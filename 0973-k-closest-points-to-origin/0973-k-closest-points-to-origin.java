2c6abe0cbe64c119f158b474b5bb587aba85b604
Varunesh Bharathiraja <23it1122@stjosephstechnology.ac.in>
2026-08-06 13:42:59 +0530

    Time: 26 ms (76.7%), Space: 54.7 MB (46.87%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,24 +1,19 @@
 class Solution {
     public int[][] kClosest(int[][] res, int k) {
         int point[][]=new int[k][2];
+        PriorityQueue<int[]>pq=new PriorityQueue<>((a,b)->a[0]-b[0]);
         for(int i=0;i<res.length;i++){
             int d1=(res[i][0]*res[i][0])+(res[i][1]*res[i][1]);
-            for(int j=i+1;j<res.length;j++){
-                int d2=(res[j][0]*res[j][0])+(res[j][1]*res[j][1]);
-                if(d1>d2){
-                    int t=res[i][0];
-                    res[i][0]=res[j][0];
-                    res[j][0]=t;
-                    t=res[i][1];
-                    res[i][1]=res[j][1];
-                    res[j][1]=t;
-                    d1=d2;
-                }
-            }
+            int arr[]=new int[3];
+            arr[0]=d1;
+            arr[1]=res[i][0];
+            arr[2]=res[i][1];
+            pq.add(arr);
         }
         for(int i=0;i<k;i++){
-            point[i][0]=res[i][0];
-            point[i][1]=res[i][1];
+            int arr[]=pq.poll();
+            point[i][0]=arr[1];
+            point[i][1]=arr[2];
         }
         return point;
     }
