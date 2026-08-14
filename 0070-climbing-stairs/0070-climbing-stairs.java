35b3e9624c088a85a5d81654035031e823091fe9
Varunesh Bharathiraja <23it1122@stjosephstechnology.ac.in>
2026-08-14 10:02:30 +0530

    Time: 0 ms (100%), Space: 41.8 MB (87.39%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int climbStairs(int n) {
+        int []arr=new int[n];
+        arr[0]=1;
+        if(n<3){
+            return n;
+        }
+        arr[1]=2;
+        for(int i=2;i<n;i++){
+            arr[i]+=arr[i-1];
+            arr[i]+=arr[i-2];
+        }
+        return arr[n-1];
+    }
+}
