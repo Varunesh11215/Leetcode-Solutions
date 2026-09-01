b0c2375770e82a8b70b0490ef62dcb1481557847
Varunesh Bharathiraja <23it1122@stjosephstechnology.ac.in>
2026-09-01 14:49:32 +0530

    Time: 18 ms (83.98%), Space: 47.3 MB (99.03%) - LeetHub

--- 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.java
+++ 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.java
@@ -1,10 +1,6 @@
 class Solution {
     public long numberOfWays(String s) {
-        long one=0;
-        long zero=0;
-        long oz=0;
-        long zo=0;
-        long ways=0;
+        long one=0,zero=0,oz=0,zo=0,ways=0;
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)=='0'){
                 zero++;
