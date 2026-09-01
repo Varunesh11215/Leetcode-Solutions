f7f32b5843a174c3df31039b35518d2eaeeb20fc
Varunesh Bharathiraja <23it1122@stjosephstechnology.ac.in>
2026-09-01 09:47:06 +0530

    Time: 106 ms (5.13%), Space: 47.1 MB (64.46%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean canFinish(int v, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>> li=new ArrayList<>();
+        for(int i=0;i<v;i++){
+            li.add(new ArrayList<>());
+        }
+        int n=prerequisites.length;
+        for(int i=0;i<n;i++){
+            li.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        }
+        int deg[]=new int[v];
+        for(int i=0;i<v;i++){
+            for(int j:li.get(i)){
+                deg[j]++;
+            }
+        }
+        int c=0;
+        Queue<Integer> q =new LinkedList<>();
+        for(int i=0;i<v;i++){
+            if(deg[i]==0){
+                q.add(i);
+            }
+        }
+        int count=0;
+        while(!q.isEmpty()){
+            int k=q.poll();
+            count++;
+            for(int i:li.get(k)){
+                deg[i]--;
+                if(deg[i]==0){
+                    q.add(i);
+                }
+            }
+        }
+        return count==v;
+
+    }
+}
