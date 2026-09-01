7d5cb80eb8619582af0eb9ffffdad72072b08b1f
Varunesh Bharathiraja <23it1122@stjosephstechnology.ac.in>
2026-09-01 21:57:47 +0530

    Time: 5 ms (65.74%), Space: 47.4 MB (5.88%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minDistance(String s1, String s2) {
+        int n=s1.length();
+        int m=s2.length();
+        int dp[][]=new int[n+1][m+1];
+        for(int i=0;i<n+1;i++){
+            dp[i][0]=i;
+        }
+        for(int i=0;i<m+1;i++){
+            dp[0][i]=i;
+        }
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else{
+                    int min=Math.min(dp[i-1][j],dp[i][j-1]);
+                    dp[i][j]=1+Math.min(min,dp[i-1][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
+
+    }
+}
